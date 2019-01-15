1345e5c27a35b0988c8ad38a42db648810fc4a29
Taras kuk <kukulyak.taras@gmail.com>
2019-01-15 13:34:52 +0200

    My first commit

--- BookingApp/Program.cs
+++ BookingApp/Program.cs
@@ -15,6 +15,7 @@ namespace BookingApp
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
