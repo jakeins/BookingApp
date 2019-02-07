32948435e2e42259eb3dfebf5e4887f58098b35f
ktv911 <kukulyak.taras@gmail.com>
2019-02-07 19:42:42 +0200

    Correct dto

--- BookingApp/DTOs/TreeGroupCrUpDto.cs
+++ BookingApp/DTOs/TreeGroupCrUpDto.cs
@@ -16,11 +16,12 @@ namespace BookingApp.DTOs
 
         [Range(1, int.MaxValue, ErrorMessage = "Invalid rule identifier.")]
         public int? DefaultRuleId { get; set; }
-        
-        
+
+        [Required]
         [MaxLength(450, ErrorMessage = "No data about User.")]
         public string CreatedUserId { get; set; }
-        
+
+        [Required]
         [MaxLength(450, ErrorMessage = "No data about User.")]
         public string UpdatedUserId { get; set; }
         
