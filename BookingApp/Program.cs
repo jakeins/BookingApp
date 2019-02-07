b8ebad5576be26a77733ca3bb464cc8740be7ce6
ktv911 <kukulyak.taras@gmail.com>
2019-02-07 17:55:08 +0200

    Edit detail

--- BookingApp/Program.cs
+++ BookingApp/Program.cs
@@ -15,7 +15,6 @@ namespace BookingApp
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
-
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
