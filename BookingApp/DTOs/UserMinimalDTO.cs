c813e749602229d2e3a5a305a1cda5db75928dca
Dxesto <dxesto@gmail.com>
2019-02-10 19:54:06 +0200

    Change UserMinimalDTO

--- BookingApp/DTOs/UserMinimalDTO.cs
+++ BookingApp/DTOs/UserMinimalDTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace BookingApp.DTOs
 {
     public class UserMinimalDto
