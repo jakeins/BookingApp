8a3e49065898117f816f75f0a768ae5048c3b4f4
Max (class) <maksyrup@gmail.com>
2019-02-13 15:13:28 +0200

    Fix occupancy stored procedure.

--- BookingApp/Data/StoredProcedures/Booking/OccupancyByResource.sql
+++ BookingApp/Data/StoredProcedures/Booking/OccupancyByResource.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE [dbo].[Resource.OccupancyPercents]	@resourceId int
 AS
 BEGIN
 	-- Check resource existence
-	if NOT EXISTS (SELECT 1 FROM [Resources] WHERE [ResourceId] = @resourceId)
+	if NOT EXISTS (SELECT 1 FROM [Resources] WHERE [Id] = @resourceId)
 		Throw 50001, 'Current entry (Resource) not found.',  12;
 
 	-- Determining basic rule properties
@@ -19,7 +19,7 @@ BEGIN
 		@serviceMinutes = COALESCE([Rule].[ServiceTime],0)
 	FROM [Resources]
 	INNER JOIN [Rules] AS [Rule] ON [Resources].[RuleId] = [Rule].[RuleId]
-	WHERE [Resources].[ResourceId] = @resourceId
+	WHERE [Resources].[Id] = @resourceId
 
 	if @ruleId IS NULL
 		Throw 50001, 'Related entry (Rule) not found.',  2;
@@ -58,7 +58,7 @@ BEGIN
 				) AS BookingDuration
 
 			FROM [Bookings]
-			WHERE [ResourceId] = @resourceId
+			WHERE [Id] = @resourceId
 			) AS MidResult
 
 		WHERE BookingDuration > 0 AND IsWierd = 0  
