0bef623cb42abd601c5a0b7ccbd7a5d288ec9819
Dxesto <dxesto@gmail.com>
2019-02-15 13:59:11 +0200

    Modify UserPasswordChangeDTO

--- BookingApp/DTOs/UserPasswordChangeDTO.cs
+++ BookingApp/DTOs/UserPasswordChangeDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace BookingApp.DTOs
 {
     public class UserPasswordChangeDTO
     {
+        [DataType(DataType.Password)]
         public string CurrentPassword { get; set; }
+        [DataType(DataType.Password)]
         public string NewPassword { get; set; }
+        [Compare("NewPassword", ErrorMessage = "Passwords don't match")]
+        public string ConfirmNewPassword { get; set; }
     }
 }
