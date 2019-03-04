c347adbc79636637cf971e796fac62067bde1f83
Ivan Andreichuk <andreeeichuk@gmail.com>
2019-03-04 22:51:51 +0200

    Add statistics for time spans with intervals

--- BookingApp/Controllers/StatisticsController.cs
+++ BookingApp/Controllers/StatisticsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using BookingApp.Services;
+using BookingApp.DTOs;
 
 namespace BookingApp.Controllers
 {
@@ -19,19 +20,71 @@ namespace BookingApp.Controllers
             resourcesService = service;
         }
 
-        [HttpGet("bpr")]
-        public async Task<IActionResult> BookingsPerResourceAllTime()
+        [HttpGet("bookings")]
+        public async Task<IActionResult> BookingsPerResource([FromQuery] DateTime? startTime, [FromQuery] DateTime? endTime, [FromQuery] string interval)
         {
+            DateTime start = startTime ?? DateTime.Now.AddYears(-1);
+            DateTime end = endTime ?? DateTime.Now;
+
             var resources = await resourcesService.ListIncludingBookings();
 
-            Dictionary<string, int> result = new Dictionary<string, int>();
+            var result = new List<BookingsPerResourceAllDTO>();
 
-            foreach(var item in resources)
+            int intervalsNumber = GetIntervalsNumber(start, end, interval)+1;            
+
+            foreach(var resource in resources)
             {
-                result.Add(item.Title, item.Bookings.Count);
-            }
+                BookingsPerResourceAllDTO dto = new BookingsPerResourceAllDTO
+                {
+                    ResourceTitle = resource.Title,
+                    BookingsPerInterval = new int[intervalsNumber]
+                };
 
+                foreach (var booking in resource.Bookings)
+                {
+                    if(booking.CreatedTime>=start&&booking.CreatedTime<=end)
+                    {
+                        int intervalID = GetIntervalsNumber(start, booking.CreatedTime, interval);
+                        dto.BookingsPerInterval[intervalID]++;
+                    }
+                }
+
+                dto.BookingsSum = dto.BookingsPerInterval.Sum();
+
+                result.Add(dto);
+            }
+            
             return Ok(result);
         }
+
+        #region Helpers
+
+        private static int GetIntervalsNumber(DateTime start, DateTime end, string interval)
+        {
+            int number = 0;
+
+            switch (interval)
+            {
+                case "month":
+                    number = (int)(end - start).TotalDays / 30;
+                    break;
+                case "week":
+                    number = (int)(end - start).TotalDays / 7;
+                    break;
+                case "day":
+                    number = (int)(end - start).TotalDays;
+                    break;
+                case "hour":
+                    number = (int)(end - start).TotalHours;
+                    break;
+                default:
+                    // error
+                    break;
+            }
+
+            return number;
+        }
+
+        #endregion
     }
 }
---
+++ BookingApp/DTOs/BookingsPerResourceAllDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApp.DTOs
+{
+    public class BookingsPerResourceAllDTO
+    {
+        public string ResourceTitle { get; set; }
+        public int[] BookingsPerInterval { get; set; }
+        public int BookingsSum { get; set; }
+    }
+}
