70e075bd595facb2762d9f2f6ef52de29f427fa5
Ivan Andreichuk <andreeeichuk@gmail.com>
2019-03-05 15:44:20 +0200

    Add some statistical data for Resources usage.

--- BookingApp/Controllers/StatisticsController.cs
+++ BookingApp/Controllers/StatisticsController.cs
@@ -54,7 +54,56 @@ namespace BookingApp.Controllers
                 return Ok(new BookingStatsDTO(stats, GetIntervalValues(start, interval, intervals)));
             }  
         }
-        
+
+        [HttpGet("resources")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(401)]
+        [Authorize(Roles = RoleTypes.Admin)]
+        public async Task<IActionResult> ResourcesUsage()
+        {
+            resources = await resourcesService.ListIncludingBookingsAndRules();
+
+            List<ResourceUsageDTO> dTOs = new List<ResourceUsageDTO>();            
+
+            foreach(var resource in resources)
+            {
+                ResourceUsageDTO dTO = new ResourceUsageDTO();
+                List<long> timeSpansinTicks = new List<long>();
+                double cancellations=0;
+
+                foreach (var booking in resource.Bookings)
+                {
+                    if(booking.TerminationTime==null||booking.TerminationTime>booking.StartTime)
+                    {
+                        timeSpansinTicks.Add((booking.EndTime - booking.StartTime).Ticks);
+                    }
+                    else
+                    {
+                        cancellations++;
+                    }
+                }
+                
+                long longAverageTicks = (long)timeSpansinTicks.Average();
+                long minTicks = timeSpansinTicks.Min();
+                long maxTicks = timeSpansinTicks.Max();
+                long modeTicks = timeSpansinTicks.GroupBy(n => n).OrderByDescending(g => g.Count()).Select(g => g.Key).FirstOrDefault();
+
+                long maxRuleTime = new TimeSpan(0, resource.Rule.MaxTime.GetValueOrDefault(),0).Ticks;
+
+                dTO.Title = resource.Title;
+                dTO.AverageTime = new TimeSpan(longAverageTicks);
+                dTO.MinTime = new TimeSpan(minTicks);
+                dTO.MaxTime = new TimeSpan(maxTicks);
+                dTO.ModeTime = new TimeSpan(modeTicks);
+                dTO.CancellationRate = cancellations / resource.Bookings.Count();
+                dTO.AverageUsageRate = (double)longAverageTicks / maxRuleTime;
+                dTOs.Add(dTO);
+            }
+
+            return Ok(dTOs);
+        }
+
         #region Helpers
 
         private int GetIntervalsNumber(DateTime start, DateTime end, string interval)
