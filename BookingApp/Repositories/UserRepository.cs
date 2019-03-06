a54dc6f9a11e1c3b77ac0c70c37f21513524f8cb
Dxesto <dxesto@gmail.com>
2019-03-06 14:01:13 +0200

    Fix user namespaces

--- BookingApp/Repositories/UserRepository.cs
+++ BookingApp/Repositories/UserRepository.cs
@@ -10,23 +10,6 @@ using System.Threading.Tasks;
 namespace BookingApp.Services
 {
 
-    public interface IUserRepository : IBasicRepositoryAsync<ApplicationUser, string>
-    {
-        Task AddUserRole(ApplicationUser user, string role);
-        Task AddUserRoles(ApplicationUser user, IEnumerable<string> roles);
-        Task ChangePassword(ApplicationUser user, string currentpassword, string newpassword);
-        Task<bool> CheckPassword(ApplicationUser user, string password);
-        Task CreateAsync(ApplicationUser user, string password);
-        Task DeleteAsync(ApplicationUser user);
-        Task<string> GeneratePasswordResetToken(ApplicationUser user);
-        Task<ApplicationUser> GetUserByEmailAsync(string email);
-        Task<IList<string>> GetUserRoles(ApplicationUser user);
-        Task<IList<string>> GetUserRolesById(string userId);
-        Task<bool> IsInRole(ApplicationUser user, string role);
-        Task RemoveUserRole(ApplicationUser user, string role);
-        Task RemoveUserRoles(ApplicationUser user, IEnumerable<string> roles);
-        Task ResetUserPassword(ApplicationUser user, string token, string newPassword);
-    }
     public class UserRepository : IUserRepository
     {
         private UserManager<ApplicationUser> userManager;
