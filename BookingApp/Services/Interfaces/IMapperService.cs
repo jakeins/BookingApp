1907a2ca79b18e0a1736f2a7a347a62634e343d8
Max <maksyrup@gmail.com>
2019-03-07 23:04:06 +0200

    Mapping service: create, declare, use in Resources.

---
+++ BookingApp/Services/Interfaces/IMapperService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BookingApp.Services.Interfaces
+{
+    public interface IMapperService
+    {
+        object Map(object source, Type sourceType, Type destinationType);
+        TDestination Map<TSource, TDestination>(TSource source);
+        TDestination Map<TDestination>(object source);
+    }
+}
---
+++ BookingApp/Services/MapperService.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BookingApp.Services.Interfaces;
+using System;
+
+namespace BookingApp.Services
+{
+    public class MapperService : IMapperService
+    {
+        readonly IMapper autoMapperInstance;
+
+        public MapperService()
+        {
+            autoMapperInstance = new Mapper(new MapperConfiguration(cfg => {
+            }));
+        }
+
+        public object Map(object source, Type sourceType, Type destinationType)
+        {
+            return autoMapperInstance.Map(source, sourceType, destinationType);
+        }
+
+        public TDestination Map<TSource, TDestination>(TSource source) => (TDestination)Map(source, typeof(TSource), typeof(TDestination));
+
+        public TDestination Map<TDestination>(object source) => (TDestination)Map(source, source.GetType(), typeof(TDestination));
+    }
+}
