1b7dead2e26eb9b57ab61e10f08ffb8750825fdc
Іван Андрейчук <andreeeichuk@gmail.com>
2019-03-07 06:38:23 +0000

    Merged PR 81: Add statistics service and controller

---
+++ BookingApp/Services/Interfaces/IStatisticsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookingApp.Entities.Statistics;
+
+namespace BookingApp.Services.Interfaces
+{
+    public interface IStatisticsService
+    {
+        Task<BookingsStats> GetBookingsCreations(DateTime start, DateTime end, string interval, int[] resourcesIDs);
+
+        Task<BookingsStats> GetBookingsCancellations(DateTime start, DateTime end, string interval, int[] resourcesIDs);
+
+        Task<BookingsStats> GetBookingsTerminations(DateTime start, DateTime end, string interval, int[] resourcesIDs);
+
+        Task<BookingsStats> GetBookingsCompletions(DateTime start, DateTime end, string interval, int[] resourcesIDs);
+
+        Task<IEnumerable<ResourceStats>> GetResourceStats();
+
+        Task<ResourceStats> GetResourceStats(int resourceID);
+    }
+}
