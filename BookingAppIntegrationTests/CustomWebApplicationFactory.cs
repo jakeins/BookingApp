6eb1a7d4f2228cec445fdd3d847430bdc3fc9138
ktv911 <kukulyak.taras@gmail.com>
2019-03-10 15:26:20 +0200

    Start integration tests

---
+++ BookingAppIntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using BookingApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace BookingAppIntegrationTests.Tests
+{
+    #region snippet1
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Create a new service provider.
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                // Add a database context (ApplicationDbContext) using an in-memory 
+                // database for testing.
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+
+
+                services.AddAuthentication(
+                    options =>
+                      {
+                          options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                          options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                      }
+                  ).AddJwtBearer(options =>
+                      {
+                          //usual config
+                      }
+                );
+
+
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                // Create a scope to obtain a reference to the database
+                // context (ApplicationDbContext).
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<ApplicationDbContext>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    // Ensure the database is created.
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        // Seed the database with test data.
+                        //Utilities.InitializeDbForTests(db);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"An error occurred seeding the " +
+                            "database with test messages. Error: {ex.Message}");
+                    }
+                }
+            });
+        }
+    }
+    #endregion
+}
---
+++ BookingAppIntegrationTests/Scenarios/FolderControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using BookingApp.Data.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace BookingAppIntegrationTests.Scenarios
+{
+    public class FolderControllerTest : IClassFixture<WebApplicationFactory<BookingApp.Startup>>
+    {
+
+        private readonly WebApplicationFactory<BookingApp.Startup> _factory;
+
+        public FolderControllerTest(WebApplicationFactory<BookingApp.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/api/folder")]
+        public async Task GetFolders(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/api/folder/1")]
+        public async Task GetFolderById(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTdXBlckFkbWluIiwiZW1haWwiOiJzdXBlcmFkbWluQGFkbWluLmNvdyIsImp0aSI6IjZjOTI4MzY0LWNkMTAtNDE5NC05ZTQzLTNhNmYwMDZjNDk0YSIsInVpZCI6IjkyZjA0Y2U3LTkwZDItNGNiMy05NmMwLWViOWE0MTNhMzA2ZCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6WyJBZG1pbiIsIlVzZXIiXSwiZXhwIjoxNTUyMjI2MzMxLCJpc3MiOiJCb29raW5nQXBwIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCJ9.-L8NN1-t-8-9Azqoxxe6Soxd5PZPeMNnTFRPqCANuVY");
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+
+
+    }
+}
