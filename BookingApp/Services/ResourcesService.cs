40a433baa6b9fff854f06332b35467e83173a98d
Max <maksyrup@gmail.com>
2019-03-11 08:11:05 +0000

    Merged PR 85: Improve server-side resourses section
    - Clean bookings controller and service from occupancy
    - Bring out occupancies aggregation to the resources service

--- BookingApp/Services/ResourcesService.cs
+++ BookingApp/Services/ResourcesService.cs
@@ -1,7 +1,9 @@
-﻿using BookingApp.Data;
-using BookingApp.Data.Models;
+﻿using BookingApp.Data.Models;
+using BookingApp.Exceptions;
+using BookingApp.Repositories;
 using BookingApp.Repositories.Interfaces;
 using BookingApp.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -10,10 +12,12 @@ namespace BookingApp.Services
     public class ResourcesService : IResourcesService
     {
         readonly IResourcesRepository resourcesRepo;
+        readonly IBookingsRepository bookingsRepo;
 
-        public ResourcesService(IResourcesRepository resourcesRepo, ApplicationDbContext dbContext)
+        public ResourcesService(IResourcesRepository resourcesRepo, IBookingsRepository bookingsRepo)
         {
             this.resourcesRepo = resourcesRepo;
+            this.bookingsRepo = bookingsRepo;
         }
 
         public async Task Create(Resource resource) => await resourcesRepo.CreateAsync(resource);
@@ -30,7 +34,42 @@ namespace BookingApp.Services
 
         public async Task<IEnumerable<Resource>> ListByAssociatedUser(string userId) => await resourcesRepo.ListByAssociatedUser(userId);
         public async Task<IEnumerable<Resource>> ListByRuleKey(int ruleId) => await resourcesRepo.ListByRuleKeyAsync(ruleId);
-        public async Task<IEnumerable<Resource>> ListByFolderKey(int folderId) => await resourcesRepo.ListByFolderKeyAsync(folderId);        
+        public async Task<IEnumerable<Resource>> ListByFolderKey(int folderId) => await resourcesRepo.ListByFolderKeyAsync(folderId);
 
+        #region Occupancy
+        public virtual async Task<double?> OccupancyByResource(int resourceId) => await bookingsRepo.OccupancyByResourceAsync(resourceId);
+
+        public async Task<Dictionary<int, double?>> GetOccupancies()
+        {
+            var idsList = await ListKeys();
+            return await GetOccupanciesByIDs(idsList);
+        }
+
+        public async Task<Dictionary<int, double?>> GetActiveOccupancies()
+        {
+            var idsList = await ListActiveKeys();
+            return await GetOccupanciesByIDs(idsList);
+        }
+
+        public async Task<Dictionary<int, double?>> GetOccupanciesByIDs(IEnumerable<int> idsList)
+        {
+            var map = new Dictionary<int, double?>();
+
+            foreach (int resourceId in idsList)
+            {
+                map.Add(resourceId, null);
+
+                try
+                {
+                    map[resourceId] = await OccupancyByResource(resourceId);
+                }
+                catch (Exception ex) when (ex is KeyNotFoundException || ex is FieldValueAbsurdException)
+                {
+                    //silently swallowing disjoint values
+                }
+            }
+            return map;
+        }
+        #endregion
     }
 }
