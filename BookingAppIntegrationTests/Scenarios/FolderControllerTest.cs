31b1f5d5d02b174095b3e2c02ceb1485d2dd9a49
Taras kuk <kukulyak.taras@gmail.com>
2019-03-12 12:04:51 +0200

    add dod checks

--- BookingAppIntegrationTests/Scenarios/FolderControllerTest.cs
+++ BookingAppIntegrationTests/Scenarios/FolderControllerTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using BookingApp.Data.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -27,6 +30,10 @@ namespace BookingAppIntegrationTests.Scenarios
             var response = await client.GetAsync(url);
 
             // Assert
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var folders = JsonConvert.DeserializeObject<IEnumerable<Folder>>(stringResponse);
+            Assert.Contains(folders, p => p.Title == "Town Hall");
+ 
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
@@ -43,11 +50,50 @@ namespace BookingAppIntegrationTests.Scenarios
             var response = await client.GetAsync(url);
 
             // Assert
+            var folder = JsonConvert.DeserializeObject<Folder>(await response.Content.ReadAsStringAsync());
+            Assert.Contains(folder.Title, "Town Hall");
+
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
 
+        [Theory]
+        [InlineData("/api/folder/122")]
+        public async Task GetFaildedFolderById(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {await this.getToken()}");
+            var response = await client.GetAsync(url);
+
+            // Assert
+
+            var statusCod = response.StatusCode.ToString();
+            Assert.Equal("NotFound", statusCod);
+        }
+
+        //[Theory]
+        //[InlineData("/api/folder/5")]
+        //public async Task GetDeleteFolder(string url)
+        //{
+        //    // Arrange
+        //    var client = _factory.CreateClient();
+
+        //    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {await this.getToken()}");
+        //    var response = await client.DeleteAsync(url);
+
+        //    // Assert
+
+        //    var statusCod = response.StatusCode.ToString();
+        //    Assert.Equal("Ok", statusCod);
+        //    response.EnsureSuccessStatusCode();
+        //}
+
+
+
+        #region Utility
         private async Task<string> getToken()
         {
             var client = _factory.CreateClient();
@@ -58,6 +104,6 @@ namespace BookingAppIntegrationTests.Scenarios
             });
             return (((responseToken.Content.ReadAsStringAsync().Result).Substring(16)).Split(',')[0]).Trim('"');
         }
-
+        #endregion Utility
     }
 }
