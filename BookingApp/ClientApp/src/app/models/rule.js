5f3bb7ab689d7daa96a21e7f6ca204beb13e6a68
en <entwos@gmail.com>
2019-03-19 00:30:18 +0200

    add rule model

---
+++ BookingApp/ClientApp/src/app/models/rule.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var rule = /** @class */ (function () {
+    function rule() {
+    }
+    return rule;
+}());
+exports.rule = rule;
+//# sourceMappingURL=rule.js.map
---
+++ BookingApp/ClientApp/src/app/models/rule.ts
@@ -0,0 +1,11 @@
+export class rule {
+  Id: number;
+  Title: string;
+  MinTime: number;
+  MaxTime: number;
+  StepTime: number;
+  ServiceTime: number;
+  PreOrderTimeLimit: number;
+  ReuseTimeout: number;
+  isActive: boolean
+}
