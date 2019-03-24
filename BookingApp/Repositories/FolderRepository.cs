569b4ecc951e2dadfa9ee8fc72bfd0ef866702cb
Taras kuk <kukulyak.taras@gmail.com>
2019-03-24 10:23:47 +0300

    Modify folder repository

--- BookingApp/Repositories/FolderRepository.cs
+++ BookingApp/Repositories/FolderRepository.cs
@@ -17,43 +17,15 @@ namespace BookingApp.Repositories
         {
         }
 
-        private async Task<Folder> GetAsync(int? id)
-        {
-            if (id == null)
-            {
-                throw NewNotFoundException;
-            }
-            else
-            {
-                return await base.GetAsync((int)id);
-            } 
-        }
-
         public override async Task UpdateAsync(Folder Folder)
         {
             await UpdateSelectiveAsync<FolderUpdateSubsetDto>(Folder);
         }
 
-        public async Task IsParentValidAsync(int? newParentId, int? currentId)
+        public async Task IsParentValidAsync(int? newParentId, int currentId)
         {
             await dbContext.Database.ExecuteSqlCommandAsync(
                     $"EXEC [Folders.IsParentValid] {newParentId}, {currentId}");
-            //var currentParentId = newParentId;
-
-            //while (true)
-            //{
-            //    if (currentParentId == null)
-            //        return true;
-            //    else
-            //    {
-            //        if (currentParentId == currentId)
-            //            throw new OperationRestrictedRelationException("Specified parent Folder can't be set because this would cause circluar dependency");
-            //        else
-            //        {
-            //            currentParentId = (await GetAsync(currentParentId)).ParentFolderId;
-            //        }
-            //    }
-            //}
         }
 
     }
