7745d765eeaec17636d2fe6475c5aaf0dd48e166
Taras kuk <kukulyak.taras@gmail.com>
2019-03-28 05:27:16 +0000

    Merged PR 92: Add unit tests UI and Stored Procedure (IsParentValid)

---
+++ BookingApp/ClientApp/src/app/models/register-form.model.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var RegisterFormModel = /** @class */ (function () {
+    function RegisterFormModel(userName, email, password, confirmPassword) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+    return RegisterFormModel;
+}());
+exports.RegisterFormModel = RegisterFormModel;
+//# sourceMappingURL=register-form.model.js.map
