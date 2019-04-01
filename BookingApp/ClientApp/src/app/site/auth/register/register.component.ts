c3157b384f8ac0f6b420c1ebded4e5b9f1cb3f5d
Taras kuk <kukulyak.taras@gmail.com>
2019-04-01 18:19:21 +0300

    Correct user ui

--- BookingApp/ClientApp/src/app/site/auth/register/register.component.ts
+++ BookingApp/ClientApp/src/app/site/auth/register/register.component.ts
@@ -7,22 +7,25 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
+  styleUrls: ['./register.component.css'],
   templateUrl: './register.component.html'
 })
 export class RegisterComponent implements OnInit {
 
   private registerForm: FormGroup;
+  private apiError: string = "";
 
   constructor(private authService: AuthService, private router: Router) {
     this.registerForm = new FormGroup({
       'userName': new FormControl('', [Validators.required, Validators.minLength(3)]),
       'email': new FormControl('', [Validators.required, Validators.email]),
       'password': new FormControl('', Validators.required),
-      'confirmPassword': new FormControl(''),
+      'confirmPassword': new FormControl('', Validators.required),
     }, { validators: this.comparePassword });
   }
 
   ngOnInit() {
+    
   }
 
   register() {
@@ -33,9 +36,7 @@ export class RegisterComponent implements OnInit {
         this.registerForm.value.password,
         this.registerForm.value.confirmPassword
       )
-    ).subscribe(data => this.router.navigate(['/login']), err => {
-      console.log(err.message);
-    });
+    ).subscribe(data => this.router.navigate(['/login']), error => this.handleError(error));
   }
 
   private comparePassword(group: FormGroup) {
@@ -43,6 +44,13 @@ export class RegisterComponent implements OnInit {
     const confirm = group.value.confirmPassword;
 
     return pass === confirm ? null : { notSame: true };
+  }
+
+  private handleError(error: any) {
+    this.apiError = error['status'];
+    if (error['error'] != undefined) {
+      this.apiError += ': ' + error['error']['Message'];
+    }
   }
 
 }
