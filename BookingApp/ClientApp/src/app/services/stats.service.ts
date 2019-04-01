f188af61e3b77528385ca517d22fe48805c3dfec
Ivan Andreichuk <andreeeichuk@gmail.com>
2019-04-01 18:45:51 +0300

    Add rest of stats client models. Add rest of client stats service methods.

--- BookingApp/ClientApp/src/app/services/stats.service.ts
+++ BookingApp/ClientApp/src/app/services/stats.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs/Observable';
 import { BASE_API_URL } from '../globals';
 import { BookingStats } from '../models/stats-booking';
+import { ResourceStats } from '../models/stats-resource';
+import { ResourceStatsBrief } from '../models/stats-resource-brief';
+import { UsersStats } from '../models/stats-users';
 import { Logger } from './logger.service';
 
 @Injectable()
@@ -26,6 +29,18 @@ export class StatsService {
     let endMonth = endString.substr(4, 3);
     let endYear = endString.substr(11, 4);
     return this.http.get<BookingStats>(this.url + `/bookings-${type}?startTime=${startDay}%20${startMonth}%20${startYear}&endTime=${endDay}%20${endMonth}%20${endYear}&interval=${interval}`);
+  }
+
+  getResourceStats(id: number): Observable<ResourceStats> {
+    return this.http.get<ResourceStats>(this.url + `/resources/${id}`);
+  }
+
+  getResourcesRating(): Observable<ResourceStatsBrief[]> {
+    return this.http.get<ResourceStatsBrief[]>(this.url + `/resources-rating`);
+  }
+
+  getUsersStats(): Observable<UsersStats> {
+    return this.http.get<UsersStats>(this.url + `/users`)
   }
 
 }
